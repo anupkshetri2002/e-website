6ffc90e56e8b63d40a549ef03af956118bdc9d2c
anupkshetri2002 <anupac2002@gmail.com>
2023-10-07 09:07:48 +0545

    final done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
                 <h1 className='mx-auto lg:mx-0 text-4xl text-bold text-yellow-500' >ARIES </h1>
                 <p className='text-gray-400 my-7 text-center lg:text-start'>Trendy Aries: Where Style Meets Sustainability. We specialize in crafting high-quality, eco-conscious clothing
                  that blends fashion-forward designs with ethical production practices
-                 <br />Kathmandu ,Baneshwor </p>
+                 <br /><span className='text-bold text-orange-800 text-2xl'>Kathmandu, Baneshwor</span> </p>
                 <div className='flex items-center gap-[18px] justify-center lg:justify-start'> 
                 <img src={facebookIcon} alt='' className='cursor-pointer' />
                 <img src={twitterIcon} alt='' className='cursor-pointer' />
@@ -26,12 +26,12 @@ const Footer = () => {
 
                 </div>
             </div>
-            <ul className='flex flex-col sm:flex-row items-center gap-10 mx-auto lg:max-0'>
-                <li className='link border-primary-green'>Home</li>
-                <li className='link border-primary-green'>Compnay</li>
-                <li className='link border-primary-green'>About</li>
-                <li className='link border-primary-green'>Resources</li>
-                <li className='link border-primary-green'>Contact</li>
+            <ul className='flex flex-col sm:flex-row items-center gap-10 mx-auto lg:max-0 text-orange-400'>
+                <li className='underline hover:text-blue-700'>Home</li>
+                <li className='underline hover:text-blue-700'>Compnay</li>
+                <li className='underline hover:text-blue-700'>About</li>
+                <li className='underline hover:text-blue-700'>Resources</li>
+                <li className='underline hover:text-blue-700'>Contact</li>
 
             </ul>
         </div>
